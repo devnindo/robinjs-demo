1395f46ecd08973bf4771edb029030e3fe5c52a6
Rashed Alam <rashed091@gmail.com>
2023-11-04 11:42:29 +0600

    testomonial to be added

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import { visualizer } from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -8,6 +9,7 @@ export default defineConfig({
       include: ['tailwind-config.cjs', 'node_modules/**'],
     },
   },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
